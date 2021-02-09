931306f8a386b8070104db86e798822ad805772e
anadelucaslopez <47280701+anadelucaslopez@users.noreply.github.com>
2021-02-09 20:25:45 +0100

    Add files via upload
    instalación windows ana

--- db_migrate.js
+++ db_migrate.js
@@ -7,7 +7,7 @@ dotenv.config();
 if (!process.env.DATABASE_URL) {
     console.error("Please define DATABASE_URL in .env file");
 } else {
-    exec(`./node_modules/.bin/sequelize db:migrate --url ${process.env.DATABASE_URL}`, function (err, stdout, stderr) {
+    exec(`npx sequelize db:migrate --url ${process.env.DATABASE_URL}`, function (err, stdout, stderr) {
         // Node.js will invoke this callback when process terminates.
         if (err) {
             console.error(err);
